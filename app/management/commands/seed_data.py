68edc4796c8173289e9a9723ee694e80c4161062
Gary Xin <gary.xin19@gmail.com>
2022-06-13 22:18:52 -0700

    Added a start time for the game

--- app/management/commands/seed_data.py
+++ app/management/commands/seed_data.py
@@ -1,85 +1,109 @@
 import random
+import datetime
 
 from django.core.management.base import BaseCommand
 
-from app.models import User, Game, Player, PlayerRole, SignupLocation, Spectator, Moderator, ParticipantRole
+from app.models import (
+    User,
+    Game,
+    Player,
+    PlayerRole,
+    SignupLocation,
+    Spectator,
+    Moderator,
+    ParticipantRole,
+)
 
-names = ['Eliseo Perlman',
-         'Hettie Pugsley',
-         'Else Vartanian',
-         'Corinna Slover',
-         'Vannessa Stultz',
-         'Dion Demott',
-         'Riva Shock',
-         'Charita Ralphs',
-         'Lillie Wannamaker',
-         'Soila Brasher',
-         'Truman Degraffenreid',
-         'Verona Broady',
-         'Larae Coker',
-         'Ernesto Mortensen',
-         'Tyesha Worden',
-         'Shondra Bondy',
-         'Britney Mirabella',
-         'Shera Klimas',
-         'Amada Milano',
-         'Emerson Buffington',
-         'Hiroko Bailer',
-         'Deeann Maher',
-         'Glen Creagh',
-         'Kurt Preble',
-         'Glenna Ringo',
-         'Debbie Frisby',
-         'Maximina Yamashita',
-         'Georgann Alsop',
-         'Jayme Hilley',
-         'Rachell Kempton',
-         'Estela Wilcoxon',
-         'Anton Spafford',
-         'Gale Legette',
-         'Tanesha Mcsorley',
-         'Kary Schmieder',
-         'Zaida Nyland',
-         'Arron Romberger',
-         'Evangeline Dagenhart',
-         'Cindy Sandor',
-         'Valencia Paschall',
-         'Sommer Thomsen',
-         'Vikki Meunier',
-         'September Froelich',
-         'Lawanda Villanveva',
-         'Cordie Jaimes',
-         'Orval Cude',
-         'Hans Buelow',
-         'Greta Heaps',
-         'Vernell Kerschner',
-         'Tory Shulman']
+names = [
+    "Eliseo Perlman",
+    "Hettie Pugsley",
+    "Else Vartanian",
+    "Corinna Slover",
+    "Vannessa Stultz",
+    "Dion Demott",
+    "Riva Shock",
+    "Charita Ralphs",
+    "Lillie Wannamaker",
+    "Soila Brasher",
+    "Truman Degraffenreid",
+    "Verona Broady",
+    "Larae Coker",
+    "Ernesto Mortensen",
+    "Tyesha Worden",
+    "Shondra Bondy",
+    "Britney Mirabella",
+    "Shera Klimas",
+    "Amada Milano",
+    "Emerson Buffington",
+    "Hiroko Bailer",
+    "Deeann Maher",
+    "Glen Creagh",
+    "Kurt Preble",
+    "Glenna Ringo",
+    "Debbie Frisby",
+    "Maximina Yamashita",
+    "Georgann Alsop",
+    "Jayme Hilley",
+    "Rachell Kempton",
+    "Estela Wilcoxon",
+    "Anton Spafford",
+    "Gale Legette",
+    "Tanesha Mcsorley",
+    "Kary Schmieder",
+    "Zaida Nyland",
+    "Arron Romberger",
+    "Evangeline Dagenhart",
+    "Cindy Sandor",
+    "Valencia Paschall",
+    "Sommer Thomsen",
+    "Vikki Meunier",
+    "September Froelich",
+    "Lawanda Villanveva",
+    "Cordie Jaimes",
+    "Orval Cude",
+    "Hans Buelow",
+    "Greta Heaps",
+    "Vernell Kerschner",
+    "Tory Shulman",
+]
 
 
 class Command(BaseCommand):
-    help = 'Generates seed data'
+    help = "Generates seed data"
 
     def handle(self, *args, **options):
-        if User.objects.filter(email='root').exists():
+        if User.objects.filter(email="root").exists():
             return
 
-        root = User.objects.create_superuser('root', 'toor', first_name='Super', last_name='User')
-        game = Game.objects.create_game('Spring 2018')
+        root = User.objects.create_superuser(
+            "root", "toor", first_name="Super", last_name="User"
+        )
+        game = Game.objects.create_game("Spring 2018", started_on=datetime.datetime.now() + datetime.timedelta(1))
 
-        SignupLocation.objects.create_signup_location('SLC', game=game)
-        SignupLocation.objects.create_signup_location('Online', game=game)
-        SignupLocation.objects.create_signup_location('EIT', game=game)
-        SignupLocation.objects.create_signup_location('DC', game=game)
+        SignupLocation.objects.create_signup_location("SLC", game=game)
+        SignupLocation.objects.create_signup_location("Online", game=game)
+        SignupLocation.objects.create_signup_location("EIT", game=game)
+        SignupLocation.objects.create_signup_location("DC", game=game)
 
         users = []
         for name in names:
-            first, last = name.split(' ')
-            user = User.objects.create_user(f'{first.lower()}@email.com', 'password', first_name=first, last_name=last)
+            first, last = name.split(" ")
+            user = User.objects.create_user(
+                f"{first.lower()}@email.com",
+                "password",
+                first_name=first,
+                last_name=last,
+            )
             users.append(user)
 
         Moderator.objects.create_moderator(root, game)
 
-        roles = [PlayerRole.HUMAN, PlayerRole.ZOMBIE, ParticipantRole.MODERATOR, ParticipantRole.SPECTATOR]
+        roles = [
+            PlayerRole.HUMAN,
+            PlayerRole.ZOMBIE,
+            ParticipantRole.MODERATOR,
+            ParticipantRole.SPECTATOR,
+        ]
         for i in range(0, 50):
             role = random.choice(roles)
             if role == ParticipantRole.MODERATOR:
@@ -89,9 +113,15 @@ class Command(BaseCommand):
             else:
                 Player.objects.create_player(user=users[i], game=game, role=role)
 
-        zombie = User.objects.create_user('zombie@email.com', 'password', first_name='Zombie', last_name='Player')
-        human = User.objects.create_user('human@email.com', 'password', first_name='Human', last_name='Player')
-        spectator = User.objects.create_user('spectator@email.com', 'password', first_name='A', last_name='Spectator')
+        zombie = User.objects.create_user(
+            "zombie@email.com", "password", first_name="Zombie", last_name="Player"
+        )
+        human = User.objects.create_user(
+            "human@email.com", "password", first_name="Human", last_name="Player"
+        )
+        spectator = User.objects.create_user(
+            "spectator@email.com", "password", first_name="A", last_name="Spectator"
+        )
 
         Player.objects.create_player(zombie, game, PlayerRole.ZOMBIE)
         Player.objects.create_player(human, game, PlayerRole.HUMAN)
